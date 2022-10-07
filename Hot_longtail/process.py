64b44489b8d6bc5ead8db719aa17cd32379c6928
[Xiyue Wang] <[xiyuewang@ucsb.edu]>
2022-10-06 20:20:57 -0700

    new code

--- Hot_longtail/process.py
+++ Hot_longtail/process.py
@@ -109,10 +109,81 @@ def split_lt_random(dataset, quantile, n=5):
 			for list in newlist:
 				for item in random.sample(list, n):
 					out_lt_train.write(item)
-				
 
-split_lt_random('yelp', 0.75)
-# def split_dataset(in_file_test, out_file_hot_test, out_file_lt_test, in_file_train, out_file_hot_train, out_file_lt_train, df, quantile):
+# Same as the above, but we want all the samples comming from the lt part.
+def split_lt_random_hot(dataset, quantile, n=5, sample = False):
+	in_file_train = 'Data/' + str(dataset) + '.train.rating'
+	out_file_hot_train = 'Data/' + str(dataset) + f'.train.hot.hotitem.rating'
+	out_file_lt_train = 'Data/' + str(dataset) + f'.train.lt.hotitem.rating'
+	df = get_df(in_file_train, 0)
+	df_item = get_df(in_file_train, 1) # get the item df
+	with open(in_file_train) as in_train:
+		threshold = df.quantile(quantile)[1]
+		threshold_item = df_item.quantile(quantile)[1]
+		with open(out_file_hot_train, 'w') as out_hot_train, open(out_file_lt_train, 'w') as out_lt_train:
+			lines_train = in_train.readlines()
+			temp = [] # Temp buffer to hold all longtail data
+			for line_train in lines_train:
+				id_train = int(line_train.split('\t')[0])
+				id_train_item = int(line_train.split('\t')[1])
+				frequency_train = df.loc[df['id'] == id_train, 'frequency'].iloc[0]
+				frequency_item = df_item.loc[df_item['id'] == id_train_item, 'frequency'].iloc[0]
+				print(f'train id:{id_train}, frequency: {frequency_train}, thres: {threshold}')
+				if frequency_train >= threshold:
+					out_hot_train.write(line_train)
+				elif frequency_item < threshold_item:
+					if sample == True:
+						temp.append(line_train)
+					else:
+						out_lt_train.write(line_train)
+			if sample == True:
+				values = set(map(lambda x:int(x.split('\t')[0]), temp))
+				newlist = [[y for y in temp if int(y.split('\t')[0])==x] for x in values]
+				# Here we perform the randomization
+				for list in newlist:
+					for item in random.sample(list, n):
+						out_lt_train.write(item)
+				
+# Given a dataset, report the number of items from hot and longtail part.
+def analyze_component(dataset, filename, quantile):
+	in_train = 'Data/' + str(dataset) + '.train.rating'
+	user_df = get_df(in_train, 0) # col_index = 0 --> userID
+	item_df = get_df(in_train, 1) # col_index = 1 --> itemID
+	user_threshold = user_df.quantile(quantile)[1]
+	item_threshold = item_df.quantile(quantile)[1]
+	hot_item_count, lt_item_count = 0, 0
+	hot_user_count, lt_user_count = 0, 0
+	num_user_id = len(user_df)
+	num_item_id = len(item_df)
+	with open(filename, 'r') as infile:
+		lines = infile.readlines()
+		for line in lines:
+			user_id = int(line.split('\t')[0])
+			item_id = int(line.split('\t')[1])
+			if (user_df['id'].eq(user_id)).any(): # Check if the user is in the random sample
+				frequency = user_df.loc[user_df['id'] == user_id, 'frequency'].iloc[0]
+				if frequency >= user_threshold:
+					hot_user_count += 1
+				else:
+					lt_user_count += 1
+			if (item_df['id'].eq(item_id)).any(): # Check if the user is in the random sample
+				frequency = item_df.loc[item_df['id'] == item_id, 'frequency'].iloc[0]
+				if frequency >= item_threshold:
+					hot_item_count += 1
+				else:
+					lt_item_count += 1
+	# Report user stats: number of userID, number of hot&lt users
+	print(f'Number of hot users: {hot_user_count}, number of lt users: {lt_user_count}')
+	print(f'Number of hot items: {hot_item_count}, number of lt items: {lt_item_count}\n')
+
+# for perc in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
+# 	dataset = 'ml-1m'
+# 	file = f'Data/{dataset}.train.hot{perc}%lt.rating'
+# 	# file = f'Hot_longtail/lesser_samples/{dataset}.train.hot{perc}%lt.rand5.rating'
+# 	analyze_component(dataset, file, 0.75)
+
+split_lt_random_hot('yelp', 0.75, sample=False)
+# split_lt_random('yelp', 0.75)
 # df = get_df(IN_FILE_PP_TRAIN, 0)
 # draw_distribution(df)
-# split_dataset(IN_FILE_PP_TEST, OUT_FILE_PP_TEST_HOT, OUT_FILE_PP_TEST_LT, IN_FILE_PP_TRAIN, OUT_FILE_PP_TRAIN_HOT, OUT_FILE_PP_TRAIN_LT, IN_FILE_PP_NEG, OUT_FILE_PP_NEG_HOT, OUT_FILE_PP_NEG_LT, df, 0.75)
+# analyze_component('yelp', 'Data/yelp.train.lt.rand5rating', 0.75)
--- Hot_longtail/visualize.py
+++ Hot_longtail/visualize.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-x_labels = ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'] # ', '40%', '50%', '60%', '70%', '80%', '90%', '100%'
 ml_hr = [0.5436, 0.5801, 0.5940, 0.6086, 0.6248, 0.6415, 0.6520, 0.6612, 0.6793, 0.6816] # 0.6612, 0.6793
 ml_ndcg = [0.2978, 0.3204, 0.3310, 0.3477, 0.3517, 0.3708, 0.3788, 0.3889, 0.4021, 0.4053] # 0.3889, 0.4021
 
@@ -16,44 +15,51 @@ yelp_ndcg = [0.4362, 0.4402, 0.4445, 0.4497, 0.4544, 0.4579, 0.4609, 0.4575, 0.4
 pp_hr = [0.3619, 0.3460, 0.3361, 0.3276, 0.3226, 0.3199, 0.3139, 0.3104, 0.3003, 0.3006] # 0.3139 0.3104 0.3003
 pp_ndcg = [0.2300, 0.2181, 0.2077, 0.2046, 0.1999, 0.1949, 0.1926, 0.1877, 0.1818, 0.1794] # 0.1926 0.1877 0.1818
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+def plot_performance(ml_data, yelp_data, pp_data, datatype):
+    x_labels = ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%']
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(x_labels, ml_data, "-o", label = 'MovieLens 1M')
+    plt.plot(x_labels, yelp_data, "-o", label = 'Yelp')
+    plt.plot(x_labels, pp_data, "-o", label = 'Amazon Prime Pantry')
+    plt.title(f"{datatype} performance of mixed datasets")
+    plt.xlabel('Percentage of longtail data mixed')
+    plt.ylabel(datatype)
+    for i, v in enumerate(ml_data):
+        ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+    for i, v in enumerate(yelp_data):
+        ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+    for i, v in enumerate(pp_data):
+        ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+    plt.legend()
+    plt.show()
 
-# plt.plot(x_labels, ml_hr, "-o", label = 'MovieLens 1M')
-# plt.plot(x_labels, yelp_hr, "-o", label = 'Yelp')
-# plt.plot(x_labels, pp_hr, "-o", label = 'Amazon Prime Pantry')
-# plt.title("HR performance of mixed datasets")
-# plt.xlabel('Percentage of longtail data mixed')
-# plt.ylabel('HR')
-# for i, v in enumerate(ml_hr):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
-# for i, v in enumerate(yelp_hr):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
-# for i, v in enumerate(pp_hr):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+ml_hot = [479843, 517152, 552738, 586181, 618039, 646365, 670732, 691755, 708151]
+ml_lt = [210916, 220892, 230303, 239101, 247404, 255141, 261659, 267139, 271203]
 
-plt.plot(x_labels, ml_hr_rand, "-o", label = 'MovieLens 1M')
-# plt.plot(x_labels, yelp_hr_rand, "-o", label = 'Yelp')
-plt.title("HR performance of mixed datasets (# lt samples = 5)")
-plt.xlabel('Percentage of longtail data mixed')
-plt.ylabel('HR')
-for i, v in enumerate(ml_hr_rand):
-    ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
-# for i, v in enumerate(yelp_hr_rand):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+yelp_hot = [325991, 348176, 369551, 390301, 411336, 431732, 451311, 469981, 487711]
+yelp_lt = [137498, 145354, 153181, 161099, 168377, 175545, 182785, 189759, 196301]
 
-# plt.plot(x_labels, ml_ndcg, "-o", label = 'MovieLens 1M')
-# plt.plot(x_labels, yelp_ndcg, "-o", label = 'Yelp')
-# plt.plot(x_labels, pp_ndcg, "-o", label = 'Amazon Prime Pantry')
-# plt.title("NDCG performance of mixed datasets")
-# plt.xlabel('Percentage of longtail data mixed')
-# plt.ylabel('NDCG')
-# for i, v in enumerate(ml_ndcg):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
-# for i, v in enumerate(yelp_ndcg):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
-# for i, v in enumerate(pp_ndcg):
-#     ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+pp_hot = [241534, 251613, 260331, 268525, 275616, 281666, 286346, 289485, 291118]
+pp_lt = [136984, 143014, 148451, 153532, 158025, 161843, 164861, 166962, 168253]
 
-plt.legend()
-plt.show()
+yelp_lesser_hot = [310720, 317723, 324765, 331787, 338872, 346019, 353032, 360033, 367053, 374207]
+yelp_lesser_lt = [132268, 134875, 137443, 140031, 142556, 145019, 147616, 150225, 152815, 155286]
+
+def plot_mixdata(hot_data, lt_data, dataset):
+    x_labels = ['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%']
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(x_labels, hot_data, "-o", label = 'Hot sample points')
+    plt.plot(x_labels, lt_data, "-o", label = 'Lt sample points')
+    plt.title(f"Number of different sample points in {dataset} dataset")
+    plt.xlabel('Percentage of longtail data mixed')
+    plt.ylabel('Samples')
+    for i, v in enumerate(hot_data):
+        ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+    for i, v in enumerate(lt_data):
+        ax.annotate(str(v), xy=(i,v), xytext=(-7,7), textcoords='offset points')
+    plt.legend()
+    plt.show()
+
+plot_mixdata(yelp_lesser_hot, yelp_lesser_lt, 'Yelp')
